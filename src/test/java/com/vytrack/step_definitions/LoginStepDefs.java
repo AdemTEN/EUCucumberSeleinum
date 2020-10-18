0e22cbfb07253b5f948b009de8d58ef66695b992
AdemTEN <marrysnm2012@gmal.com>
2020-10-18 19:49:12 +0200

    created LoginStepDefs class

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -8,20 +8,17 @@ public class LoginStepDefs {
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("I open browser and navigate to vytrack login page");
     }
 
     @When("the user enters the driver information")
     public void the_user_enters_the_driver_information() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("I enter username and password");
     }
 
     @Then("the user should be able to login")
     public void the_user_should_be_able_to_login() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("I verify that title changed to Dashboard");
     }
 
 
