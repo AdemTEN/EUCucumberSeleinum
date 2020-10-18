3c10bdf6721c7feb5e83e5202737fd1c1fe99bce
AdemTEN <marrysnm2012@gmal.com>
2020-10-18 18:31:44 +0200

    created LoginStepDefs class

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -1,6 +1,19 @@
 package com.vytrack.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features"
+)
+
+
+
 public class CukesRunner {
+
+
 
 
 }
